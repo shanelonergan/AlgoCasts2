61680c1ef62dfb0d0fadca23ffd2bf1858ff5850
shanelonergan <sptlonergan@gmail.com>
2020-05-04 11:02:15 -0400

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,13 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {}
+
+    str.split('').forEach(char => charMap[char] = charMap[char] + 1 || 1)
+    console.log(charMap)
+
+    return Object.keys(charMap).reduce((max, char) => charMap[char] > charMap[max] ? char : max)
+}
 
 module.exports = maxChar;
