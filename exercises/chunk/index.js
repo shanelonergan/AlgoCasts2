fc8e664134dbfb99e9d56e998ff68572355b204f
shanelonergan <sptlonergan@gmail.com>
2020-05-05 10:23:49 -0400

    array chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,22 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let output = []
+    let chunkArr = []
+
+    for (let i = 0; i < array.length; i++) {
+
+        chunkArr.push(array[i])
+
+        if (chunkArr.length === size || i === array.length - 1) {
+            output.push(chunkArr)
+            chunkArr = []
+        }
+    }
+
+    console.log(output, size)
+    return output
+}
 
 module.exports = chunk;
